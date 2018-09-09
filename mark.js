9a95a336a0131029f6824a832cd824a9a0c69a46
JianJiao <jianjiao321@gmail.com>
2018-09-08 18:11:39 -0700

    mod: update with full list

--- mark.js
+++ mark.js
@@ -1,7 +1,25 @@
 ;{
-  const vals = [896, 891, 888, 873, 870, 867, 849, 840, 835, 832, 830, 825, 795, 792, 775, 769, 755, 747]
-  const set = new Set(vals.map(val => String(val)))
-  const $els = $('td').filter((index, el)=> set.has($(el).attr('value')))
+  /*
+   highlight some elements by adding outline
+  */
+  const include = new Set([426, 427, 428, 429, 430, 431, 433, 457, 458, 470, 489, 518, 558, 559, 589, 590, 622, 641, 700, 701, 702, 703, 704, 705, 706, 707, 708, 709, 710, 747, 751, 752, 754, 755, 756])
+  const exclude = new Set([757, 759, 761, 765, 768, 770, 772, 773, 774, 778, 780, 782, 786, 793, 798, 803, 805, 810, 815, 827, 828, 834, 839, 843, 847, 850])
+  const $els = $('td').filter((i, el) => {
+    const val = $(el).attr('value') || ''
+    if (val.includes('.')) {
+      return false
+    }
+    const num = Number.parseInt(val, 10)
+    if (Number.isFinite(num)) {
+      if (num <= 756) {
+        return include.has(num)
+      } else {
+        return !exclude.has(num)
+      }
+    } else {
+      return false
+    }
+  })
   $els.css('outline', '1px solid red')
 }
 
